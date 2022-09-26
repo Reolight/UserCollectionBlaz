e52b4ba23615db6255f9e36f202ed8b83f818a6e
Reolight <95131769+Reolight@users.noreply.github.com>
2022-09-27 00:46:47 +0300

    #32. Some security changes

--- Areas/Identity/Data/AppDbContext.cs
+++ Areas/Identity/Data/AppDbContext.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using UserCollectionBlaz.ViewModel;
 
 namespace UserCollectionBlaz.Areas.Identity.Data;
 
--- Service/EmailService.cs
+++ Service/EmailService.cs
@@ -1,25 +1,28 @@
 ﻿using System.Net;
 using System.Net.Mail;
 using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
+using UserCollectionBlaz.Data;
 
 namespace UserCollectionBlaz.Service;
 
 public class EmailService : IEmailSender
 {
     private readonly SmtpClient _smtpClient;
-
-    public EmailService()
+    private readonly string _email;
+    public EmailService(IOptions<EmailSettings> options)
     {
-        _smtpClient = new SmtpClient("smtp.gmail.com", 587);
-        _smtpClient.Credentials = new NetworkCredential("reolightmiene@gmail.com", "wwhpcujwkpmdvfgi");
+        _smtpClient = new SmtpClient(options.Value.Host, 587);
+        _smtpClient.Credentials = new NetworkCredential(options.Value.Email, options.Value.Password);
         _smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
         _smtpClient.EnableSsl = true;
+        _email = options.Value.Email;
     }
 
     public async Task SendEmailAsync(string email, string subject, string htmlMessage)
     {
         MailMessage mail = new MailMessage();
-        mail.From = new MailAddress("reolightmiene@gmail.com", subject);
+        mail.From = new MailAddress(_email, subject);
         mail.To.Add(new MailAddress(email));
         mail.Body = htmlMessage;
         mail.IsBodyHtml = true;
